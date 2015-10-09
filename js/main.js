d139f9ac41996e926db193d5995ef9bc53847568
Sen-Zhang <senzhang1987@126.com>
2015-10-09 15:54:41 -0700

    add partials

--- js/main.js
+++ js/main.js
@@ -35,6 +35,39 @@ myBlog.config(function ($stateProvider, $urlRouterProvider) {
         },
         'labels@bvg': {
           templateUrl: 'js/templates/articles/bootstrap_view_generator/labels.html'
+        },
+        'panels@bvg': {
+          templateUrl: 'js/templates/articles/bootstrap_view_generator/panels.html'
+        },
+        'panelRows@bvg': {
+          templateUrl: 'js/templates/articles/bootstrap_view_generator/panel_rows.html'
+        },
+        'modals@bvg': {
+          templateUrl: 'js/templates/articles/bootstrap_view_generator/modals.html'
+        },
+        'navs@bvg': {
+          templateUrl: 'js/templates/articles/bootstrap_view_generator/navs.html'
+        },
+        'navbars@bvg': {
+          templateUrl: 'js/templates/articles/bootstrap_view_generator/navbars.html'
+        },
+        'dropdowns@bvg': {
+          templateUrl: 'js/templates/articles/bootstrap_view_generator/dropdowns.html'
+        },
+        'icons@bvg': {
+          templateUrl: 'js/templates/articles/bootstrap_view_generator/icons.html'
+        },
+        'progressBars@bvg': {
+          templateUrl: 'js/templates/articles/bootstrap_view_generator/progress_bars.html'
+        },
+        'breadcrumbs@bvg': {
+          templateUrl: 'js/templates/articles/bootstrap_view_generator/breadcrumbs.html'
+        },
+        'buttons@bvg': {
+          templateUrl: 'js/templates/articles/bootstrap_view_generator/buttons.html'
+        },
+        'forms@bvg': {
+          templateUrl: 'js/templates/articles/bootstrap_view_generator/forms.html'
         }
       }
     });
