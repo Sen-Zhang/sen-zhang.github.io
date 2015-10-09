ade20308cd5cdc4875a96c125d16c8fea0ee1fbb
Sen-Zhang <senzhang1987@126.com>
2015-10-09 14:45:31 -0700

    add badge helper docs

--- js/main.js
+++ js/main.js
@@ -29,6 +29,12 @@ myBlog.config(function ($stateProvider, $urlRouterProvider) {
         },
         'alerts@bvg': {
           templateUrl: 'js/templates/articles/bootstrap_view_generator/alerts.html'
+        },
+        'badges@bvg': {
+          templateUrl: 'js/templates/articles/bootstrap_view_generator/badges.html'
+        },
+        'labels@bvg': {
+          templateUrl: 'js/templates/articles/bootstrap_view_generator/labels.html'
         }
       }
     });
